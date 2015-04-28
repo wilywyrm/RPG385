3a0b865c5a3e1a8ffc1bfa2ae1fea41b084254aa
Rufei Zhou <rufei.z@gmail.com>
2015-04-27 21:13:45 -0500

    got press enter to continue to work

--- src/rpg385.c
+++ src/rpg385.c
@@ -10,13 +10,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include "data.h"
 
 int week = 0;
 int health = 100;
 int hygiene = 100;
+int happiness = 100;
 int grades = 100;
 int difficulty = 0;
+
 
 
 char* eventTexts[] = {
@@ -24,21 +27,20 @@ char* eventTexts[] = {
 		"",
 };
 
-int main(void) {
-	//puts("ayy lmao"); // prints !!!Hello World!!!
-	//printf("ayy lmao\n");
-	prints(eventTexts[0]);
+char* choiceTexts[] = {
+		"",
+		"",
+		"",
+		"TURN UP"
+};
 
-	while(week < 15){
-		int userSel;
-		drawScreen();
-		prints("What do you choose? ");
-		userSel = getIn();
-		week++;
-	}
+char* choiceResponses[] = {
+		"Welcome to RPG385!\n",
+		"",
+};
 
-	return EXIT_SUCCESS;
-}
+Event* e[16];
+
 
 Event* Event_new(int eventTextI, int choiceTextI[], int choiceResponsesI[], int choiceNextEventI[]){
 	Event* e = malloc(sizeof(Event));
@@ -56,18 +58,31 @@ Event* Event_new(int eventTextI, int choiceTextI[], int choiceResponsesI[], int 
 	return e;
 }
 
+void initEvents(){
+	int* cTI;	// choice text indices
+	int* cRI;	// choice response indices
+	int* cNI;	// choice next event indices
+
+	cTI = (int[4]) {1, 2, 3, 4};
+	cRI = (int[4]) {1, 2, 3, 4};
+	cNI = (int[4]) {1, 1, 1, 1};
+	e[0] = Event_new(0, cTI, cRI, cNI);
+}
+
 void prints(const char *format, ...){
 	va_list arg;
 	printf(format, arg);
 }
 
-int getIn(){
-	char buf[80] = "boo false value";
+char getIn(){
+	//prints("What do you choose? ");
 
-	scanf("%s", buf);
+	char buf[80] = "boo invalid value";
+	//scanf("%s", buf);
 
 	while(strlen(buf) != 1){
 		drawScreen();
+		prints("What do you choose? ");
 		scanf("%s", buf);
 	}
 
@@ -76,5 +91,31 @@ int getIn(){
 }
 
 void drawScreen(){
+	int i;
+	for(i = 0; i < 20; i++){
+		printf("\n");
+	}
+}
 
+
+int main(void) {
+	initEvents();
+
+	prints(eventTexts[0]);
+
+	while(week < 15){
+		char userSel[80];
+		//drawScreen();
+		userSel[0] = getIn();
+		printf("Press enter to continue... ");
+		while(getchar() == "\n"){
+			userSel[0] = userSel[0];
+			//userSel[0] = getchar();
+		}//while(userSel[0] == "\n");
+		getchar();
+
+		week++;
+	}
+
+	return EXIT_SUCCESS;
 }
