49769cde9c6f695d606c7f35a2b44452db3925c5
Rufei Zhou <rufei.z@gmail.com>
2015-05-04 00:26:55 -0500

    add events

--- src/data.h
+++ src/data.h
@@ -7,6 +7,7 @@ typedef struct Choice {
 	int textI;
 	int responseI;
 	int nextEventI;
+	int impacts[3];
 } Choice;
 
 typedef struct Event {
@@ -17,19 +18,42 @@ typedef struct Event {
 
 
 const char* eventTexts[] = {
-		"Welcome to RPG385!",
+		"You wake up and look out to see a blanket of snow about a foot thick. \nThe chancellor sent out an email letting you know that class was NOT canceled today. \nWelcome back to campus.",
+		"You forget about your 385 Lab this week and miss it entirely, so you reschedule to go to the Thursday 3PM section. \nYou need to pick a TA. ",
+		"Career fair! Sadly, it cuts into all your classes for Tuesday. ",
+		"Surprise, midterms already! ",
+		"IEEE and ACM have had a falling out after a friendly coding competition. \nBeing a member of both, you are stuck in the middle.\n You need to pick a side in this nerdy gang war.",
 		"",
+		"It is pitch black. You are likely to be eaten by a grue.",
+		"",
+		"",
+		"SURPRISE! 385 final is next Monday!",
 };
 
 const char* choiceTexts[] = {
-		"as",
-		"sd",
-		"df",
-		"TURN UP"
+		"Stay in bed. It's chillabus week, right?",
+		"Contemplate the wisdom of your decision to go to this school",
+		"Trudge out into the frozen wasteland and go to class",
+		"TURN UP", // Turning up is always an option
+		"Stay in bed",
+		"Go anyway",
+		"Go ice skating with your buds",
+		"Go to Cravings",
+		"Pick IEEE",
+		"Pick ACM",
+		"Screw this, start your own RSO with blackjack and hookers",
+		"Grab some red bull from County and study your ass off",
+		"Watch Netflix in bed."
 };
 
 const char* choiceResponses[] = {
 		"\n",
+		"I mean, it's not like you're paying to go here or anything.",
+		"This couldn't possibly go wrong.",
+		"Obviously the only real choice.",
+		"You must be fun at parties, huh?",
+		"Big mistake...",
+		"Nice, it's what Jesus would do.",
 		"How original.",
 };
 
--- src/rpg385.c
+++ src/rpg385.c
@@ -15,28 +15,30 @@
 
 int week = 0;
 
-int health = 100;
-int hygiene = 100;
 int happiness = 100;
+int hygiene = 100;
 int grades = 100;
 
 int difficulty = 0;
 
 
-Event* ev[16];
+Event* ev[10];
 Event* curE;
 
-Event* Event_new(int eventTextI, int choiceTextI[], int choiceResponsesI[], int choiceNextEventI[]){
+Event* Event_new(int eventTextI, int choiceTextI[], int choiceResponsesI[], int choiceNextEventI[], int choiceImpacts[]){
 	Event* e = malloc(sizeof(Event));
 	e->promptI = eventTextI;
 	e->choices = malloc(4 * sizeof(Choice));
 
 	int i;
 	Choice* c = e->choices;
-	for(i = 0; i < 4; i++){
-		c[i].textI = choiceTextI[i];
-		c[i].responseI = choiceResponsesI[i];
-		c[i].nextEventI = choiceNextEventI[i];
+	for(i = 0; i < 4; i++){ // we use nextEventI to mark if this choice is possible (if -1, don't show the choice)
+			c[i].textI = choiceTextI[i];
+			c[i].responseI = choiceResponsesI[i];
+			c[i].nextEventI = choiceNextEventI[i];
+			int a;
+			for(a = 0; a < 3; a++)
+				c[i].impacts[a] = choiceImpacts[i*3 + a];
 	}
 
 	//printf("%i\n", c[0].textI);
@@ -48,16 +50,17 @@ void initEvents(){
 	int* cTI;	// choice text indices
 	int* cRI;	// choice response indices
 	int* cNI;	// choice next event indices
+	int* cI	;	// choice impacts (on happiness, hygiene, grades)
 
 	cTI = (int[4]) {0, 1, 2, 3};
 	cRI = (int[4]) {0, 1, 2, 3};
 	cNI = (int[4]) {1, 1, 1, 1};
-	ev[0] = Event_new(0, cTI, cRI, cNI);
-	//int i = 0;
-	//Choice* a = ev[0]->choices;
-	//for(i = 0; i < 4; i++){
-	//	prints("%i %c. %s\n", i, (char)(i+97), choiceTexts[(a[i]).textI]);
-	//}
+	cI  = (int[12]) { 	0, 0, 0,
+						0, 0, 0,
+						0, 0, 0,
+						0, 0, 0 };
+	ev[0] = Event_new(0, cTI, cRI, cNI, cI);
+
 }
 
 void prints(const char *format, ...){
@@ -71,17 +74,18 @@ void printChoices(Event* e){
 	int i = 0;
 	Choice* a = e->choices;
 	for(i = 0; i < 4; i++){
-		prints("%c. %s\n", (char)(i+97), choiceTexts[(a[i]).textI]);
+		if(a[i].nextEventI != -1) // we don't print a choice with nextEvent index of -1
+			prints("%c. %s\n", (char)(i+97), choiceTexts[(a[i]).textI]);
 	}
 	prints("\n\n");
 }
 
-char getIn(){
+char getIn(Event* e){
 	char buf[80] = "boo invalid value";
 	//scanf("%s", buf);
 
 	while(strlen(buf) != 1){
-		drawScreen();
+		drawScreen(e);
 		prints("What do you choose? ");
 		scanf("%s", buf);
 	}
@@ -89,24 +93,25 @@ char getIn(){
 	return buf[0];
 }
 
-void drawScreen(){
+void drawScreen(Event* e){
 	int i;
 	for(i = 0; i < 20; i++){
-		printf("\n");
+		prints("\n");
 	}
 
-	printf("Week %i: \n\n", week);
-	printf("%s\n\n", eventTexts[curE->promptI]);
-	printChoices(curE);
+	prints("Week %i: \n\n", week+1);
+	prints("%s\n\n", eventTexts[e->promptI]);
+	printChoices(e);
 }
 
 void waitEnter(){
-	printf("Press enter to continue... ");
+	prints("Press enter to continue... ");	// prompt
 	int i = 0;
-	while(getchar() == "\n"){
+	char buf[50];
+	while(getchar() != '\n'){	// wait for any preceding scanf() to flush out stdin buffer
 		i++;
 	}
-	getchar();
+	fgets(buf, 10, stdin);					// actual wait for Enter (or any other key really, we don't check)
 }
 
 
@@ -114,12 +119,17 @@ int main(void) {
 	initEvents();
 	curE = ev[0];
 
-	prints(eventTexts[0]);
+	prints("Welcome to RPG385!\n");
 
-	while(week < 15){
+	waitEnter();
+
+	while(week < 10){
 		char userSel;
 		//drawScreen();
-		userSel = getIn();
+		userSel = getIn(curE);
+		userSel -= 97; // turn ascii lowercase char into index num
+		curE = ev[(int) (curE->choices)[userSel].nextEventI];
+
 		waitEnter();
 		week++;
 	}
